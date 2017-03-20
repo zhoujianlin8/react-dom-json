2b7bf60ba50955bd6a3132b1c8614ce4a9ac0c28
jianlin.zjl <jianlin.zjl@alibaba-inc.com>
2017-03-20 21:26:35 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+import "./index.scss";
+class ReactJsonDom extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+
+        };
+    }
+    render() {
+        return (
+        <div className="reactjsondom-component">
+            hello world
+        </div>);
+    }
+    componentDidMount(){}
+}
+
+export default ReactJsonDom;
+
